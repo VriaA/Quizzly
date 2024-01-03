cb7ea3ce1d215311f338f89a3e3b813666f3209f
VriaA <oyelolaabimbola23@gmail.com>
2024-01-03 22:03:10 +0100

    improve error handling in getQuestions function

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -55,6 +55,7 @@ function App() {
     e.preventDefault()
     const formData = new FormData(e.target)
     const {category, difficulty, type} = Object.fromEntries(formData)
+    
     getQuestions(category, difficulty, type)
   }
 
@@ -62,13 +63,37 @@ function App() {
       try {
         setLoading(true)
         const response = await fetch(`https://opentdb.com/api.php?amount=5${category !== 'random' ? `&category=${category}` : ''}${difficulty !== 'random' ? `&difficulty=${difficulty}` : ''}${type !== 'random' ? `&type=${type}` : ''}`)
+        handleResponseError(response)
+
         const data = await response.json()
+        handleDataError(data)
         console.log(data)
       } catch(error) {
-        alert('Error starting quiz.')
+        alert(error.message)
       } finally {
         setLoading(false)
       }
+  }
+
+  function handleResponseError(response) {
+    if(!response.ok) {
+      if(response.status >= 500) {
+        throw new Error('Error starting quiz. Unable to connect to the server.')
+      } else {
+        throw new Error('Error starting quiz. Please check your internet connection and try again')
+      }
+    }
+  }
+
+  function handleDataError(data) {
+    const RESPONSE_CODE_NO_RESULTS = 1;
+    const RESPONSE_CODE_TOO_MANY_REQUESTS = 5;
+
+    if (data.response_code === RESPONSE_CODE_NO_RESULTS) {
+      throw new Error('No results found: The server returned no quiz questions.');
+    } else if (data.response_code === RESPONSE_CODE_TOO_MANY_REQUESTS) {
+      throw new Error('Too many requests: Please refresh the page and try again later.');
+    }
   }
 
   return (
