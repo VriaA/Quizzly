8bb9aa62678216a6f0a4474a882faba6eaabf65f
VriaA <oyelolaabimbola23@gmail.com>
2024-01-04 23:40:15 +0100

    change theme button className

--- quizzical/src/components/Header.jsx
+++ quizzical/src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(props) {
             <h1>Quizzical</h1>
             
             <button 
-                className={`theme-icon-cntr ${theme === 'dark' && 'button-dark'}`}
+                className={`change-theme-btn ${theme === 'dark' && 'button-dark'}`}
                 onClick={changeTheme}>
                 <span id="theme-icon" className='theme-icon material-symbols-outlined'>
                     {theme === 'dark' ? 'light_mode' : 'dark_mode'}
