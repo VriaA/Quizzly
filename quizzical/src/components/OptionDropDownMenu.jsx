16c8908970146ea01d00e54497c1ba6db3309730
VriaA <oyelolaabimbola23@gmail.com>
2024-01-07 23:55:20 +0100

    Add isOpen state and handleTriggerClick function.
    The isOpen state stores an object with Boolean values that represent
    whether a dropdown menu is open or not.
    
    The handleTriggerClick function is passed to an onChange event listener
    on each dropdown menu trigger and flips its corresponding Boolean value
    in the isOpen object.
    
    The isOpen value of a dropdown menu is used to toggle the visibility of
    that dropdown menu, set the aria-expanded attribute value, and rotate
    the arrow of the dropdown trigger

--- quizzical/src/components/OptionDropDownMenu.jsx
+++ quizzical/src/components/OptionDropDownMenu.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react"
 import quizOptions from "../data/quizOptions"
 
 export default function OptionDropdownMenu(props) {
       const {menuName, MenuOptions, selectedOption, setSelectedOption, isDarkTheme} = props
+
+      const [isOpen, setIsOpen] = useState({
+        categoryDropdown: false,
+        difficultyDropdown: false,
+        typeDropdown: false
+      })
       
       function handleSelectionChange(e) {
         const optionRadioInput = e.target
@@ -10,6 +17,17 @@ export default function OptionDropdownMenu(props) {
         })
       }
 
+      function handleTriggerClick(e) {
+        const triggerId = e.currentTarget.id
+
+        setIsOpen(prev=> {
+          return {...prev, [`${triggerId}Dropdown`]: !prev[`${triggerId}Dropdown`]}
+        })
+      }
+
+      const DROPDOWN_HIDDEN_CLASS = isOpen[`${menuName}Dropdown`] ? '' : 'hidden'
+      const ROTATE_TRIGGER_ARROW_CLASS = isOpen[`${menuName}Dropdown`] ? 'rotate-arrow' : ''
+
       return (
           <div className={`options-wrapper ${menuName}-options`}>
             <button 
@@ -17,17 +35,18 @@ export default function OptionDropdownMenu(props) {
               className={`info-trigger ${menuName}-trigger ${isDarkTheme && 'button-dark info-trigger-dark'}`} 
               type="button"
               aria-haspopup='listbox' 
-              aria-expanded='false' 
-              aria-controls={`${menuName}-dropdown`}>
+              aria-expanded={isOpen[`${menuName}Dropdown`]} 
+              aria-controls={`${menuName}-dropdown`}
+              onClick={handleTriggerClick}>
 
               <span id={`${menuName}-trigger-name`} className={`${menuName}-trigger-name trigger-name`}>{menuName}</span> 
-              <span id={`${menuName}-trigger-arrow`} className="expand-arrow material-symbols-outlined">
+              <span id={`${menuName}-trigger-arrow`} className={`${ROTATE_TRIGGER_ARROW_CLASS} expand-arrow material-symbols-outlined`}>
                 expand_more
               </span>
 
             </button>
 
-            <ul id={`${menuName}-dropdown`} className={`hidden info-dropdown ${isDarkTheme && 'info-dropdown-dark'}`} role='listbox' aria-labelledby={`${menuName}-trigger-name`}>
+            <ul id={`${menuName}-dropdown`} className={`${DROPDOWN_HIDDEN_CLASS} info-dropdown ${isDarkTheme && 'info-dropdown-dark'}`} role='listbox' aria-labelledby={`${menuName}-trigger-name`}>
               <MenuOptions 
                 name={menuName}
                 options={quizOptions[menuName]}
