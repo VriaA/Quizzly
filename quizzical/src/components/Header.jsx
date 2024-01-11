ec922b8b10f301ec9df2150c5492542cc1daa02e
VriaA <oyelolaabimbola23@gmail.com>
2024-01-11 04:11:53 +0100

    Make theme button accessible for screen readers
    - Add the 'aria-label' attribute to the change theme button.
    - Add a <span> element with 'aria-live' and 'aria-label'
      attributes for announcing theme changes.

--- quizzical/src/components/Header.jsx
+++ quizzical/src/components/Header.jsx
@@ -4,12 +4,13 @@ export default function Header(props) {
         <header>
             <h1>Quizzical</h1>
             
-            <button 
+            <button aria-label="Change theme"
                 className={`change-theme-btn ${theme === 'dark' && 'button-dark'}`}
                 onClick={changeTheme}>
                 <span id="theme-icon" className='theme-icon material-symbols-outlined'>
                     {theme === 'dark' ? 'light_mode' : 'dark_mode'}
                 </span>
+                <span aria-live="polite" aria-label={theme === 'dark' ? 'Theme changed to dark' : 'Theme changed to light'}></span>
             </button>
         </header>
     )
