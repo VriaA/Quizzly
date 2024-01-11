e068fca026ebb5f256e39cce78a3cf2c2bf5af15
VriaA <oyelolaabimbola23@gmail.com>
2024-01-11 23:23:07 +0100

    add 'aria-selected' attribute to <li>
    - to allow screen readers to know which option was selected in
      the category dropdown menu

--- quizzical/src/components/CategoryOption.jsx
+++ quizzical/src/components/CategoryOption.jsx
@@ -28,7 +28,7 @@ export default function CategoryOption(props) {
             const {id, name} = category
 
             return (
-                    <li role='option' key={i}>
+                    <li role='option' aria-selected={selectedOption.category === name} key={i}>
                         <input 
                             id={`category-${name}`} 
                             className='info-option' 
