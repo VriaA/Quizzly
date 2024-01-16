46eab0843433175c55d284b9252290f025d9c1a9
VriaA <oyelolaabimbola23@gmail.com>
2024-01-16 02:56:05 +0100

    refactor shuffleAnswers function

--- quizzical/src/components/Quiz.jsx
+++ quizzical/src/components/Quiz.jsx
@@ -50,7 +50,7 @@ export default function Questions(props) {
 
     function shuffleAnswers(answers) {
         for (let i = answers.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i));
+            const j = Math.floor(Math.random() * (i + 1));
             [answers[i], answers[j]] = [answers[j], answers[i]];
         }
         return answers
