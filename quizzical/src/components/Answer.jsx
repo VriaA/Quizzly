128f521938095898afc47c84f7a4a18b38fa71a5
VriaA <oyelolaabimbola23@gmail.com>
2024-01-17 02:19:01 +0100

    update answer label className

--- quizzical/src/components/Answer.jsx
+++ quizzical/src/components/Answer.jsx
@@ -3,9 +3,9 @@ export default function Answer(props) {
 
     function styleAnswerInSolution(isSelected, isCorrect) {
         if (isSolution && isSelected && !isCorrect) {
-            return 'wrong-answer'
+            return ' wrong-answer'
         } else if (isSolution && isCorrect) {
-            return 'right-answer'
+            return ' right-answer'
         }
     }
 
@@ -17,7 +17,7 @@ export default function Answer(props) {
                 <label 
                     key={i} 
                     htmlFor={`question-${questionIndex}-option-${i + 1}`} 
-                    className={`answer-label${isDarkTheme ? ' quiz-form-dark' : ''}${isRadioDisabled ? ' disabled-option' : ''}${ styleAnswerInSolution(isSelected, isCorrect) || ''}`}>
+                    className={`answer-label${isDarkTheme ? ' quiz-form-dark' : ''}${isRadioDisabled ? ' disabled-option' : ''}${styleAnswerInSolution(isSelected, isCorrect) || ''}`}>
                     <input
                         id={`question-${questionIndex}-option-${i + 1}`} 
                         className="answer-radio" 
