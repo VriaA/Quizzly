28c988cad8c334dc12085e15e1d370a5e22e7c97
VriaA <oyelolaabimbola23@gmail.com>
2024-01-25 00:48:50 +0100

    Adjust countdown timer
    - Add timer icon for visual representation
    - Add indicator to notify when the quiz is about to end
    - Adjust timer range to start from 0 and end at 75 seconds
    - Ensure the timer remains in view at all times

--- quizzly/src/components/QuizDetails.jsx
+++ quizzly/src/components/QuizDetails.jsx
@@ -4,39 +4,73 @@ import beepSoundEffect from '../assets/music/beep_beep.mp3'
 export default function QuizDetails(props) {
     const beepSound = new Audio(beepSoundEffect)
     const {loading, isResult, setIsResult, isSolution, isDarkTheme, selectedOption} = props
-    const [timeLeft, setTimeLeft] = useState(90)
+    const [timeLeft, setTimeLeft] = useState(0)
     const [timeSpent, setTimeSpent] = useState({secondsSpent: "00", minutesSpent: "00"})
     const [isTimeUp, setIsTimeUp] = useState(false)    
+
+    // CHANGES THE POSITION OF THE COUNTDOWN TIMER TO FIXED WHEN THE QUIZ DETAILS SECTION IS AT THE TOP OF THE SCREEN OR NO LONGER IN VIEW.
+    useEffect(()=> {
+        window.addEventListener('scroll', _=> {
+            const countdownWrapper = document.getElementById("countdown-wrapper")
+            const quizDetailsRect = document.getElementById("quiz-details").getBoundingClientRect()
+            const isAtTop = quizDetailsRect.top <= 0
+
+            if(countdownWrapper && isAtTop) {
+                countdownWrapper.classList.add("fixed-countdown")
+            } else {
+                countdownWrapper.classList.remove("fixed-countdown")
+            }
+        })
+    }, [])
 
     // KEEPS TRACK OF THE TIME LEFT AND TIME SPENT EVERY SECOND DURING A QUIZ
     useEffect(()=>{
         const updateTimeLeft = setInterval(_=> {
             if(!loading && !isResult && !isSolution && !isTimeUp) {
-                setTimeLeft(prevTimeLeft=> prevTimeLeft - 1)
+                setTimeLeft(prevTimeLeft=> prevTimeLeft + 1)
                 updateTimeSpent()   
             }
         }, 1000)
         return ()=> clearInterval(updateTimeLeft)
     }, [loading, isResult, isSolution, isTimeUp])
 
-    // SETS 'isTimeUp' TO TRUE ONCE THE TIME LEFT FOR A QUIZ IS LESS THAN OR EQUAL TO 0
+    /*- UPDATES THE COUNTDOWN VALUE.
+      - ENDS THE QUIZ WHEN THE TIME IS UP
+      - STYLE THE ENDING COUNTDOWN*/
     useEffect(()=> {
         const countdown = document.getElementById('countdown')
         if(!countdown) return
         countdown.value = timeLeft
-        if(timeLeft <= 0) {
+
+        endQuizAtTimeUp()
+        styleEndingCountdown()
+    }, [timeLeft])
+
+    // SETS 'isTimeUp' TO TRUE ONCE THE TIME LEFT FOR A QUIZ IS GREATER THAN OR EQUAL TO 75
+    // PLAYS A BEEP SOUND WHEN THE TIME IS UP
+    function endQuizAtTimeUp() {
+        if(timeLeft >= 75) {
             setIsTimeUp(true)
             beepSound.play()
         }
-    }, [timeLeft])
+    }
+
+    function styleEndingCountdown() {
+        if(timeLeft >= 65) {
+            countdown.classList.add("countdown-ending-soon")
+        }
+    }
 
     // SHOWS THE QUIZ RESULTS WHEN THE TIME FOR A QUIZ IS UP BY SETTING 'isResult' TO TRUE
+    // REMOVES 'countdown-ending-soon' FROM TIMER COUNTDOWN
     useEffect(()=> {
+        const countdown = document.getElementById("countdown")
         if(isTimeUp) {
             setTimeout(()=> {
                 setIsResult(true)
                 beepSound.currentTime = 0
             }, 2500)
+            countdown.classList.remove("countdown-ending-soon")
         }
     }, [isTimeUp])
 
@@ -69,14 +103,19 @@ export default function QuizDetails(props) {
     // USED TO RENDER A MESSAGE WHEN THE TIME FOR A QUIZ IS UP
     const isTimeUpMessageVisible = isTimeUp && !isSolution && !isResult
     return (
-        <section className="quiz-details">
+        <section id="quiz-details" className="quiz-details">
             <h2>{preferredCategory}</h2>
             <div className="quiz-details-inner">
                 {/* SHOWS THE TIME SPENT IF THE QUIZ SOLUTION IS BEING DISPLAYED.
                     SHOWS THE QUIZ COUNTDOWN TIMER IF THE QUIZ IS STILL ONGOING */}
                 {isSolution ? 
                     <p className={`time-spent ${isDarkTheme && 'time-spent-dark'}`}>Time spent {`${timeSpent.minutesSpent}:${timeSpent.secondsSpent}`}</p> 
-                    : <progress id="countdown" className={`countdown ${isDarkTheme && 'countdown-dark'}`} value={90} min={0} max={90}>80</progress>
+                    :<div id="countdown-wrapper" className="countdown-wrapper">
+                        <span className="timer-icon material-symbols-outlined">
+                            timer
+                        </span>
+                        <progress id="countdown" className={`countdown ${isDarkTheme && 'countdown-dark'}`} value={timeLeft} min={0} max={75}>{timeLeft}</progress>
+                     </div>
                 }
                 <p className="preferred-difficulty-and-type"><span>{preferredDifficulty}</span> | <span>{preferredType}</span></p>
                 <p className={`time-up-message ${isTimeUpMessageVisible && 'show-time-up-message'} ${isDarkTheme && 'time-up-message-dark'}`}>Time's up!</p>
