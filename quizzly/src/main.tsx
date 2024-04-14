35944e040d6d8f67e6baf93e6823a66223a9a416
VriaA <oyelolaabimbola23@gmail.com>
2024-04-14 03:57:57 +0100

    refactor main.tsx & move module declarations

--- quizzly/src/main.tsx
+++ quizzly/src/main.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render( <App /> )
+const rootElement = document.getElementById('root') as HTMLElement
+ReactDOM.createRoot(rootElement).render( <App /> )
